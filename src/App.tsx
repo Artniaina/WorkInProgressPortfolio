e86b56e5518dc4259d2bcf36f754f1bab03c5156
Artniaina <kanto.andriahariniaina@gmail.com>
2025-03-19 11:36:31 +0300

    Update: start from 0 again

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Skills from './components/Skills';
-import Hero from './components/Hero';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="min-h-screen bg-cyber-dark text-white">
-      <div className='h-[90vh]'>
-      <Header />
-      <main className="max-w-6xl mx-auto px-6 py-20 " >
-      <Hero />  
-      </main>
-      </div>
-  
-      <section className="bg-cyber-purple/10 py-20">
-      <Skills />
-      </section>
+  const [count, setCount] = useState(0)
 
-      <Footer />
-    </div>
-  );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
