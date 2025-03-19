97ea69e06c654d73a13b6daa1b079ba01655fdbb
Artniaina <kanto.andriahariniaina@gmail.com>
2025-03-19 17:17:48 +0300

    Update: Add animated GIF(test)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,55 +1,73 @@
 import { motion } from 'framer-motion';
 import { ReactTyped } from 'react-typed';
+import image from '../assets/testt.gif';
 const Hero = () => {
   return (
-    <div className="max-w-8xl px-6 py-20 flex justify-start">
-    <div className="flex flex-col items-center">
-      <motion.h1 
-        className="text-4xl md:text-6xl font-pixel retro-shadow mb-8 text-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-      >
+    <div className="max-w-8xl px-6 py-20 flex justify-start relative">
+      <div className="flex flex-col items-center">
+        <motion.h1
+          className="text-4xl md:text-6xl font-pixel retro-shadow mb-8 text-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
         <span className="glitch-effect" data-text="I AM KANTO">I AM KANTO </span>
-        <br />
-        <span className="text-cyber-pink text-[1.7rem] mt-7 flex justify-start align-center ml-10">
-          <ReactTyped
-            strings={[
-              "A FULLSTACK DEVELOPER",
-              "A TECH ENTHUSIAST",
-              "A LIFELONG LEARNER",
-            ]}
-            typeSpeed={50}
-            backSpeed={30}
-            loop
-          />
-        </span>
-      </motion.h1>
-      
-      <motion.p 
-        className="text-lg mb-12 text-cyber-light  text-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.5 }}
-      >
-        I am a web and mobile developer from Madagascar. <br /> 
-        Continuously learning, evolving, <br />and refining my craft every single day.
-      </motion.p>
 
-      <motion.div 
-        className="flex gap-6"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.8 }}
-      >
-            <a href="#contact" className="px-8 py-4 border-2 border-cyber-pink bg-cyber-purple hover:bg-cyber-pink transition-colors rounded">
-             Download CV
-            </a>
-             <a href="#projects" className="px-8 py-4 bg-cyber-purple hover:bg-cyber-pink transition-colors rounded pixel-border">
-              Contact me
-            </a>
-      </motion.div>
-    </div>
+          <br />
+          <span className="text-cyber-pink text-[1.7rem] mt-7 flex justify-start align-center ml-10">
+            <ReactTyped
+              strings={[
+                "A FULLSTACK DEVELOPER",
+                "A TECH ENTHUSIAST",
+                "A LIFELONG LEARNER",
+              ]}
+              typeSpeed={50}
+              backSpeed={30}
+              loop
+            />
+          </span>
+        </motion.h1>
+
+        <motion.p
+          className="text-lg mb-12 text-cyber-light text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5 }}
+        >
+          I am a web and mobile developer from Madagascar. <br />
+          Continuously learning, evolving, <br />
+          and refining my craft every single day.
+        </motion.p>
+
+        <motion.div
+          className="flex gap-6"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.8 }}
+        >
+          <a
+            href="#contact"
+            className="px-8 py-4 border-2 border-cyber-pink bg-cyber-purple hover:bg-cyber-pink transition-colors rounded"
+          >
+            Download CV
+          </a>
+          <a
+            href="#projects"
+            className="px-8 py-4 bg-cyber-purple hover:bg-cyber-pink transition-colors rounded pixel-border"
+          >
+            Contact me
+          </a>
+        </motion.div>
+      </div>
+
+      <div className="absolute bottom-5 right-20">
+        <img
+          src={image}
+          alt="Animated GIF"
+          className="w-[25rem] h-[25rem] object-cover"
+       
+        />
+      </div>
     </div>
   );
 };
