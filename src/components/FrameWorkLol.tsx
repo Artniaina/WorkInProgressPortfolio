7cd7fad60c4c39721d783734a85d5059e1732fc8
Artniaina <kanto.andriahariniaina@gmail.com>
2025-03-21 10:34:33 +0300

    Update: Revise skills

--- src/components/FrameWorkLol.tsx
+++ src/components/FrameWorkLol.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 
 const FrameWorkLol = () => {
-    const skills: string[] = ['Teamwork', 'Fast Selflearning', 'Versatility'];
+    const skills: string[] = ['Adaptability', 'Fast Selflearning', 'Versatility','Continuous Improvement Enthusiast'];
 
     return (
         <div>
@@ -13,7 +13,7 @@ const FrameWorkLol = () => {
             >
                 <div className="h-8 bg-purple-600 border-b border-purple-400 px-2 flex justify-between items-center"></div>
                 <div className="h-8 bg-purple-600 border-b border-purple-400 px-2 flex justify-between items-center">
-                    <div className="text-white">APresentation.txt</div>
+                    <div className="text-white">APresentationOfMeXD.txt</div>
                     <div className="flex space-x-2">
                         <div className="w-4 h-4 bg-gray-900 border border-purple-300 flex items-center justify-center">
                             <div className="w-2 h-2 bg-purple-300"></div>
@@ -35,16 +35,16 @@ const FrameWorkLol = () => {
                         <div className="text-xl mb-2 text-cyber-pink">Education and Background:</div>
                         <div className="pl-4 mb-4">
                             <div className="mb-2">
-                                • Bachelor's Degree in Computer Science: Integration and Development at "École Supérieure des
-                                Technologies de l'Information" (2022-2025)
+                                • Bachelor's Degree in Computer Science: Integration and Development at ESTI : "École Supérieure des
+                                Technologies de l'Information " (2022-2025)
                             </div>
-                            <div className="mb-2">• English Courses at "Madagascar Professional Training Centre"</div>
-                            <div className="mb-2">• Scientific Baccalaureate at Lycée "Les Bambinos II"</div>
+                            <div className="mb-2">• English Courses at "Madagascar Professional Training Centre" (2021)</div>
+                            <div className="mb-2">• Scientific Baccalaureate at "Les Bambinos II" (2021)</div>
                         </div>
                     </div>
 
                     <div>
-                        <div className="text-xl mb-2 text-cyber-pink"> Skills:</div>
+                        <div className="text-xl mb-2 text-cyber-pink"> Soft skills:</div>
                         <div className="pl-4">
                             {skills.map((skill, index) => (
                                 <motion.div
@@ -61,15 +61,15 @@ const FrameWorkLol = () => {
                     </div>
                 </motion.div>
 
-                {/* Footer */}
+            
                 <motion.div
                     className="h-8 bg-purple-700 border-t border-purple-500 flex justify-between items-center px-2"
                     initial={{ y: 50, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
                     transition={{ delay: 0.5, duration: 0.5 }}
                 >
-                    <button className="w-6 h-6 bg-gray-900 border border-purple-400 text-purple-300 flex items-center justify-center">
-                        1
+                    <button className="w-12 h-6 bg-gray-900 border border-purple-400 text-purple-300 flex items-center justify-center">
+                        1/1
                     </button>
                     <div className="flex-grow mx-2">
                         <div className="w-full h-4 bg-purple-600 rounded"></div>
